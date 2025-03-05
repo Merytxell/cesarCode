cdde7324d46848758c54e8b2bd5a029d7d0d4e29
AvilaC <avila.claire@gmail.com>
2025-03-05 14:23:22 +0100

    add args

--- cesar.py
+++ cesar.py
@@ -1,22 +1,24 @@
-# message= "c'est donc ça c’est bien sur l’incroyable bazar dont j’ai vague mémoire à mon sommeil troublé"
-# cle=24
-
+import sys
 
 def cesar_code(text, key):
-
-    messageCrypted=""
+    messageCrypted = ""
 
     for c in text:
-        if 'a' <= c <='z':
-            newLetter = chr(((ord(c) - ord('a')+ key)%26)+ord('a'))
+        if 'a' <= c <= 'z':
+            newLetter = chr(((ord(c) - ord('a') + key) % 26) + ord('a'))
             messageCrypted += newLetter
+        else:
+            messageCrypted += c
 
-    else:
-       messageCrypted += c
-       return messageCrypted
+    return messageCrypted
 
-message = input("Entrez le message à chiffrer :")
-key = int(input("Entrez la clé : "))
+# Vérifier si des arguments sont passés en ligne de commande
+if len(sys.argv) > 2:
+    message = sys.argv[1]
+    key = int(sys.argv[2])
+else:
+    message = input("Entrez le message à chiffrer : ")
+    key = int(input("Entrez la clé : "))
 
-cryptedText = cesar_code(message,key)
-print(cryptedText)
+cryptedText = cesar_code(message, key)
+print("Texte chiffré :", cryptedText)
